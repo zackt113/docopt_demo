70cbe83ffd7c8b1e75600100429b325adefddfab
Zack Tang <zacktang113@gmail.com>
2021-11-21 00:24:59 -0800

    update script

--- demo.py
+++ demo.py
@@ -8,9 +8,11 @@ Options:
 <arg1>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
 """ 
 
 from docopt import docopt
 opt = docopt(__doc__)
 print(opt)
 print(type(opt))
+print(opt["<arg4>"])
